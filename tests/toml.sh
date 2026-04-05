97f8e3e7d2d7b9f32abf2665102ff68560347505
TheProgrammingSauce <thepsauce@fensterchef.org>
2026-04-05 10:45:17 +0200

    remove binary file
    Now the binary file is no longer put into tests/ but into build/.

--- tests/toml.sh
+++ tests/toml.sh
@@ -2,15 +2,17 @@
 
 make -f tests/toml/GNUmakefile || exit
 
+run=build/tests/toml/run
+
 for f in tests/toml/valid/* ; do
-    if ! ./tests/toml/run "$f" ; then
+    if ! "./$run" "$f" ; then
         echo "test failed on '$f'"
         exit 1
     fi
 done
 
 for f in tests/toml/invalid/* ; do
-    if ./tests/toml/run "$f" >/dev/null ; then
+    if "./$run" "$f" >/dev/null ; then
         echo "test failed on '$f'"
         exit 1
     fi
