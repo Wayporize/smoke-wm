53c067346ba60dae6dfe8c86b3faf23cea98164b
TheProgrammingSauce <thepsauce@fensterchef.org>
2026-04-13 18:52:01 +0200

    rename test to be in line with the code file names

--- tests/all.sh
+++ tests/all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-for f in usage home toml configuration configuration-path bindings ; do
+for f in usage home toml configuration configuration-path binding ; do
     "./tests/$f.sh" && echo "$f tests succeeded" || exit
 done
